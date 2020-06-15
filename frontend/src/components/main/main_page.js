4d9ac5e203b83e7947686a257f655d278b9a7d7f
pauchye <olga.a.smi@gmail.com>
2020-06-14 22:12:00 -0400

    fixed frontend

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Save The Date</h1>
+        <footer>
+          Copyright &copy; 2020 Save The Date
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
