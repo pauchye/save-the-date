dc159288abc0e025e11c3cb1da8bcb39172dee6c
pauchye <olga.a.smi@gmail.com>
2020-06-15 14:54:26 -0400

    map is on the dash

---
+++ frontend/build/precache-manifest.f0e4fd7494ffc7fe125363b303f4642e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f368e1d2dbea5a67ccd0c958a21c925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eb2674ea1beb9420b10",
+    "url": "/static/js/2.d064aff9.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.d064aff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273efd55004ca995e646",
+    "url": "/static/js/main.8d582f91.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
