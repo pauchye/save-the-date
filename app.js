aa391059f9625cdda536d63f6766198386bdfba1
pauchye <olga.a.smi@gmail.com>
2020-06-14 20:23:04 -0400

    heroku

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
+// for heroku
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 // middleware for the body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
