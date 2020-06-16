36d97b37d75911bdb8b75000992ae293493b1ef3
pauchye <olga.a.smi@gmail.com>
2020-06-16 19:14:14 -0400

    remove console.log from secrets output

--- config/secrets.js
+++ config/secrets.js
@@ -1,10 +1,8 @@
 const {mongoURI, secretOrKey, googleMapKey} = process.env;
 
 if (mongoURI) {
-    console.log('secrets from env, we\'re in heroku!');
     module.exports = {mongoURI, secretOrKey, googleMapKey}
 } else {
-    console.log('secrets from local file, we\'re in local!');
     module.exports = require('./keys.js')
 }
 
