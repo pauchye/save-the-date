aa58d39d705f2ddd8588090ddd07a86b83d5a77b
AndyJia4700 <61808688+AndyJia4700@users.noreply.github.com>
2020-06-18 18:17:29 -0400

    Delete secrets.js

--- frontend/src/secrets.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  mongoURI: 'mongodb+srv://dev:UXh5kReX9QZ7r63k@cluster0.4dlx0.mongodb.net/db?retryWrites=true&w=majority',
-  secretOrKey: 'QRbvpOpohjeLXPYmNpKUKMKUulEac5wC',
-}
-window.secrets = config;
-export default config;
