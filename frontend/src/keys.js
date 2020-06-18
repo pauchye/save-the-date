4f8285fb7a13db0b73f3a6f2f2a4bdbd0d97b2f6
AndyJia4700 <61808688+AndyJia4700@users.noreply.github.com>
2020-06-18 18:16:56 -0400

    Delete keys.js

--- frontend/src/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:UXh5kReX9QZ7r63k@cluster0.4dlx0.mongodb.net/db?retryWrites=true&w=majority",
-  secretOrKey: "QRbvpOpohjeLXPYmNpKUKMKUulEac5wC",
-  googleMapKey: "AIzaSyBXsdt7pBaD-tiMFIksB9ctNs-t7CxRB20",
-};
